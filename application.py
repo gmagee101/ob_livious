ed3812c15f49d1dc3d698bceb888d571e2377489
Gabrielle Trotter <gtrotter@bulogics.com>
2017-10-10 18:17:42 -0400

    Data argument in URL will show up on page

--- application.py
+++ application.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello, World!'
+    dataEntered = ""
+    if 'data' in request.args:
+        dataEntered = request.args.get('data')
+        return dataEntered
+    else:
+        return "No Data"
 
 @app.route('/home')
 def home():
